32ebe0136f6cd359fdeab3f10896f14c6b145741
Maximilien Louis Dal Viallon <maximilien.viallon@gmail.com>
2020-06-15 00:29:09 +0200

    adding project to github repo

---
+++ OxbridgeProject/OxbridgeProject/OxbridgeProject/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using OxbridgeProject.View;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace OxbridgeProject
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            
+           MainPage = new NavigationPage(new LoginView());
+
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ OxbridgeProject/OxbridgeProject/OxbridgeProject/Model/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OxbridgeProject.Model
+{
+    class User
+    {
+        public string email { get; set; }
+        public string password { get; set; }
+        public string _id { get; set; }
+        public string teamName { get; set; }
+        public Boolean isBroadcasting { get; set; }
+    }
+}
